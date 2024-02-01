cfad8ee99fb9438bfebdf32209eaf569bfed8ec6
WebAlexB <belousalek2@gmail.com>
2024-02-01 19:45:47 +0200

    fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,5 @@
 const arrNum = ['   123456  ',' -0123456  ','     -12345  ','   -023456',' -000016   ',' -000000123456  ',' - 000000123456  ',' +000123456  '];
+const regex = /^\s*[-+]?0*([1-9]\d{5}|0{6})\s*$/;
+const result = arrNum.map(str => regex.test(str) ? 'так' : 'ні');
 
-const regularNums = arrNum.map(arr => {
-    const numericValue = parseFloat(arr.replace(/^\s*([-+]?[0-9]+)\s*$/, '$1'));
-    const isValid = numericValue !== 0 && numericValue % 123456 === 0;
-    return isValid ? 'так' : 'ні';
-});
-
-console.log(regularNums);
+console.log(result);
